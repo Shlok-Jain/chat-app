ab91a34192be117d003ad1f3cefcbb67e360bec1
Shlok-Jain <rubikscubeshlokjain2012@gmail.com>
2021-09-12 19:01:15 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const app = express()
+const server = require('http').createServer(app)
+const io = require("socket.io")(server, {
+    cors: {
+      origin: "*",
+    },
+  });
+
+
+app.get('/',(req,res)=>{res.sendFile(__dirname+'/index.html')})
+app.get('/script.js',(req,res)=>{res.sendFile(__dirname+'/script.js')})
+
+
+const users = {}
+
+io.on('connection',socket=>{
+    socket.on('new-user',name1=>{
+        users[socket.id] = name1
+        socket.broadcast.emit('user-connected',name1)
+    })
+    socket.on('send-chat-message',message=>{
+        var obj = {name1:users[socket.id],message:message}
+        socket.broadcast.emit('chat-message',obj)
+    })
+    socket.on('disconnect', () => {
+        socket.broadcast.emit('user-disconnected', users[socket.id])
+        delete users[socket.id]
+      })
+})
+
+server.listen(3000)
